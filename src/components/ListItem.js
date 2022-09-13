7d322e459638c4269897e9fd00780495bda2e650
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-09-13 20:29:10 +0300

    add getTitle function

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,3 +4,14 @@ import { Link } from 'react-router-dom'
 let getTime = (note) => {
     return new Date(note.updated).toLocaleDateString()
 }
+
+let getTitle = (note) => {
+    //spit by new lines and just get the first line
+    //split will make a list of each line and will only pull on the first line by index zero
+
+    const title = note.body.split('\n')[0]
+    if (title.length > 45) {
+        return title.slice(0, 45)
+    }
+    return title
+}
