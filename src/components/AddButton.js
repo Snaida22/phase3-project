439ff3f0627bd273bd5e0286d321353b7a7c3f88
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-09-13 20:20:18 +0300

    add button function

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { ReactComponent as AddIcon} from '../Assets/add.svg'
 
-function AddButton() {
+function AddBtton() {
   return (
-    <div>AddButton</div>
-  )
+    <Link to='/note/new' className='floating-button'>
+       <AddIcon />
+    </Link>
+
+
+    )
 }
 
-export default AddButton
+export default AddBtton
